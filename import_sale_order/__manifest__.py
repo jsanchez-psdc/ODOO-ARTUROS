52d74ee4a1abe3834e283f6d597771f53f838456
jsanchez-psdc <47335169+jsanchez-psdc@users.noreply.github.com>
2019-04-29 09:02:36 -0500

    ADD MODULE import_sales_order
    import_sales_order

---
+++ import_sale_order/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Import Sale Order from Excel or CSV File',
+    'version': '12.0.0.4',
+    'sequence': 4,
+    'category': 'Sales',
+    'summary': 'Easy to Import multiple sales order with multiple sales order lines on Odoo by Using CSV/XLS file',
+    'description': """
+	BrowseInfo developed a new odoo/OpenERP module apps.
+	This module use for import bulk Sales from Excel file. Import Sale order lines from CSV or Excel file.
+	Import Sale, Import Sale order line, Import Sales, Import SO. Sale Import, Add SO from Excel.Add Excel Sale order.Add CSV file.Import Sale data. Import excel file
+
+BrowseInfo desarrolló una nueva aplicación de módulo odoo / OpenERP.Este módulo se usa para importar ventas masivas de archivos de Excel. Importar líneas de pedido de venta desde CSV o archivo de Excel.Venta de importación, línea de orden de venta de importación, ventas de importación, importación SO. Importación de venta, agregue SO de Excel.Agregue orden de venta de Excel. Agregue archivo CSV. Importe de datos de venta. Importar archivo de Excel
+
+وضعت BrowseInfo تطبيقات الوحدة الجديدة Openoo / OpenERP.تستخدم هذه الوحدة لاستيراد مبيعات مجمعة من ملف Excel. استيراد خطوط ترتيب البيع من ملف CSV أو Excel.استيراد بيع ، استيراد ترتيب بيع الخط ، استيراد المبيعات ، استيراد SO. بيع استيراد ، إضافة SO من Excel.Add Excel Sale order.Add CSV file.Import بيع البيانات. استيراد ملف اكسل
+
+BrowseInfo a développé une nouvelle application de module odoo / OpenERP.
+Ce module est utilisé pour importer des ventes groupées à partir d'un fichier Excel. Importer des lignes de commande de vente à partir d'un fichier CSV ou Excel.
+Vente d'importation, ligne de commande d'importation de vente, ventes à l'importation, importation SO. Vente Import, Ajouter SO à partir de Excel.Add Excel Ordre de vente.Ajouter fichier CSV.Import données de vente. Importer un fichier Excel
+
+BrowseInfo desenvolveu um novo aplicativo odoo / OpenERP.
+Este módulo usa para importar vendas a granel do arquivo do Excel. Linhas de ordem de importação de vendas de arquivos CSV ou Excel.
+Venda de importação, linha de pedidos de importação, importação de vendas, importação SO. Importação de vendas, adicione SO de Excel. Adicione o pedido de venda do Excel. Adicione o arquivo CSV. Dados de venda de importação. Importar arquivo excel
+	-
+    """,
+    'author': 'BrowseInfo',
+    'website': 'http://www.browseinfo.in',
+    "price": 25,
+    "currency": 'EUR',
+    'depends': ['base','sale','sale_management'],
+    'data': ["sale.xml"
+            ],
+	'qweb': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'live_test_url':'https://youtu.be/VEjYx4fASZs',
+    "images":['static/description/Banner.png'],
+}
